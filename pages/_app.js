4b34c22b154aa4c659e3c704e4fb8e2ca37c9993
Rin <linhdinhlhpk97@gmail.com>
2024-05-12 20:50:39 +0700

    fix _app.js lan 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import { NextUIProvider } from "@nextui-org/react";
-import "../styles/global.css";
-function MyApp({ Component, pageProps }) {
-  return (
-    // Use at the root of our app
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
-}
-export default MyApp;
+// import { NextUIProvider } from "@nextui-org/react";
+// import "src/app/styles/global.css";
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     // Use at the root of our app
+//     <NextUIProvider>
+//       <Component {...pageProps} />
+//     </NextUIProvider>
+//   );
+// }
+// export default MyApp;
